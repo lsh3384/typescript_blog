742b761a55f0bfe487a08ffbbf0c49c0342e347c
lee <lsh3384@gmail.com>
2023-04-25 23:08:59 +0900

    1페이지 pdf 업로드 후 미리보기

--- client/src/PdfToJpgConverter.tsx
+++ client/src/PdfToJpgConverter.tsx
@@ -1,56 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Document, Page, pdfjs} from "react-pdf";
 import { PDFDocumentProxy } from 'pdfjs-dist/types/src/display/api';
-import { createCanvas } from "canvas";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const PdfToJpgConverter = () => {
-  const [numPages, setNumPages] = useState(0);
-  const [pdf, setPdf] = useState<any>(null); // pdf 모듈 추가
 
-  const onDocumentLoadSuccess = ({numPages} : PDFDocumentProxy) => {
-    console.log("onDocumentLoadSuccess!");
-    setNumPages(numPages);
-    setPdf(pdf); // pdf 모듈 설정
-  };
+function FileUpload() {
+  const [file, setFile] = useState<File | null>(null);
+  const [pdfData, setPdfData] = useState<string>('');
 
-  // const convertToJpg = async () => {
-  //   const canvas = createCanvas(1000, 1000); // create a canvas with a width and height
-  //   const ctx = canvas.getContext("2d"); // get canvas context
+  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
+  }
 
-  //   for (let i = 1; i <= numPages; i++) {
-  //     const pageCanvas = await getPageCanvas(i);
-  //     ctx.drawImage(pageCanvas, 0, 0, canvas.width, canvas.height); // draw the page canvas on the main canvas
-  //     const imageDataUrl = canvas.toDataURL("image/jpeg"); // convert canvas to image data URL
-  //     // Do something with the image data URL, like upload it to server or display it on screen
-  //   }
-  // };
-
-  // const getPageCanvas = async (pageNumber: number) => {
-  //   const page = await pdf.getPage(pageNumber); // pdf 모듈 사용
-  //   const viewport = page.getViewport({ scale: 1 });
-  //   const canvas = createCanvas(viewport.width, viewport.height);
-  //   const ctx = canvas.getContext("2d");
-  //   const renderContext = {
-  //     canvasContext: ctx,
-  //     viewport: viewport,
-  //   };
-  //   await page.render(renderContext).promise;
-  //   return canvas;
-  // };
+  useEffect(() => {
+    async function loadPdfData() {
+      if (file) {
+        const dataUrl = await blobToBase64(file);
+        setPdfData(dataUrl);
+      }
+    }
+    loadPdfData();
+  }, [file]);
 
   return (
     <div>
-      <Document
-        file="http://localhost:5000/sample.pdf"
+      <input type="file" onChange={handleFileChange} />
+      {pdfData && (
+        <Document file={pdfData} onLoadError={console.error}>
+          <Page pageNumber={1} renderTextLayer={false}/>
+        </Document>
+      )}
+    </div>
+  );
+}
+
+function blobToBase64(blob: Blob): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = () => {
+      const base64Data = reader.result?.toString();
+      if (base64Data) {
+        resolve(base64Data);
+      } else {
+        reject(new Error('Failed to convert Blob to base64'));
+      }
+    };
+    reader.onerror = () => {
+      reject(reader.error);
+    };
+  });
+}
+
+const PdfToJpgConverter = () => {
+  const [numPages, setNumPages] = useState(0);
+  const [file, setFile] = useState({url: "http://localhost:5000/sample.pdf"})
+  const onDocumentLoadSuccess = ({numPages} : PDFDocumentProxy) => {
+    console.log("onDocumentLoadSuccess");
+    setNumPages(numPages);
+  };
+
+  return (
+    <div>
+      {/* <Document
+        file={file}
         onLoadSuccess={onDocumentLoadSuccess} // pdf 모듈 전달
       >
         <Page pageNumber={1} />
-        {/* {Array.from(new Array(numPages), (el, index) => (
+        {Array.from(new Array(numPages), (el, index) => (
           <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-        ))} */}
-      </Document>
+        ))}
+      </Document> */}
+     <FileUpload></FileUpload>
       {/* <button onClick={convertToJpg}>Convert to JPG</button> */}
     </div>
   );
