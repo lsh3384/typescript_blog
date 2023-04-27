f5d86cfff603e16a776bd9f879a51f3e5a90623f
lee <lsh3384@gmail.com>
2023-04-27 23:42:20 +0900

    failed to convert pdf base64 to jpeg using image object

--- client/src/PdfToJpgConverter.tsx
+++ client/src/PdfToJpgConverter.tsx
@@ -10,6 +10,37 @@ function PdfToJpgConverter() {
   const [pdfData, setPdfData] = useState<string>('');
   const [numPages, setNumPages] = useState(0);
 
+  function convertToJpg() {
+    console.log("convertToJpg");
+    const img = new Image();
+    
+    console.log(img);
+
+    const canvas = document.createElement("canvas");
+
+    // Set the canvas width and height to match the image
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    // Draw the image onto the canvas
+    const ctx = canvas.getContext("2d");
+    if(ctx) {
+      ctx.drawImage(img, 0, 0);
+    }
+
+    // Get the canvas data as a data URI
+    const dataUri = canvas.toDataURL();
+
+    // Do something with the data URI
+    console.log(dataUri);
+    
+    img.onload = () => {
+      // Create a new canvas element
+      console.log("img.onload success!");
+    };
+    img.src = pdfData;
+  }
+
   const onDocumentLoadSuccess = ({numPages} : PDFDocumentProxy) => {
     console.log("onDocumentLoadSuccess, numPages");
     console.log(numPages);
@@ -34,6 +65,7 @@ function PdfToJpgConverter() {
     loadPdfData();
   }, [file]);
 
+  
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
@@ -46,6 +78,8 @@ function PdfToJpgConverter() {
           ))}
         </Document>
       )}
+      <img src={pdfData}></img>
+      <button onClick={() => convertToJpg()}>변환</button>
     </div>
   );
 }
