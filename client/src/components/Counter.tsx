4b8df23717ee8ab7f42e234a076fed385dcb3107
lee <lsh3384@gmail.com>
2023-05-07 23:16:20 +0900

    adding minus button

--- client/src/components/Counter.tsx
+++ client/src/components/Counter.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { increment } from '../features/counter/counterSlice';
+import { decrement, increment } from '../features/counter/counterSlice';
 import { RootState } from '../store';
 
 const Counter = () => {
     const counterValue = useSelector((state: RootState) => state.counter.value);
     const dispatch = useDispatch();
-  const handlePluseCounter = () => dispatch(increment());
+  const handlePlusCounter = () => dispatch(increment());
+  const handleMinusCounter = () => dispatch(decrement());
 	return (
 		<>
 			<p>counterValue : {counterValue}</p>
-            <button onClick={handlePluseCounter}>counter 증가</button>
+            <button onClick={handlePlusCounter}>counter 증가</button>
+            <button onClick={handleMinusCounter}>counter 감소</button>
 		</>
 	);
 };
 
 export default Counter;
+
