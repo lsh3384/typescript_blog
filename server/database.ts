060d05360d526f1d05429af136153b300b4f12b3
lee <lsh3384@gmail.com>
2023-05-12 23:52:22 +0900

    applying mysql

---
+++ server/database.ts
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const mysql = require('mysql2');
+
+let connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'blog'
+});
+
+module.exports = connection;
--- server/router/test.ts
+++ server/router/test.ts
@@ -1,8 +1,29 @@
 const express = require("express");
+const dbConnection = require("../database");
+
 const router = express.Router();
+import { RowDataPacket } from "mysql2"
+
+export interface User extends RowDataPacket {
+  idx: number
+  id: string
+  name: string
+}
+
 
 router.get("/", (req: any, res: any) => {
-    console.log("/api/")
+    dbConnection.query<User[]>(
+        'SELECT * FROM `user` WHERE `name` = ?',
+        ['lee'],
+        function(err, results, fields) {
+          console.log(results); // results contains rows returned by server
+          console.log(fields); // fields contains extra meta data about results, if available
+      
+          // If you execute same statement again, it will be picked from a LRU cache
+          // which will save query preparation time and give better performance
+        }
+      );
+    
     res.send({ test : "hi"});
 })
 
