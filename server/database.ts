d75ff29015d22aa720b5ba89226a81e99ead7dc0
lee <lsh3384@gmail.com>
2023-05-13 00:12:26 +0900

    applying mysql2 completed

--- server/database.ts
+++ server/database.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const mysql = require('mysql2');
+import { QueryError,RowDataPacket } from "mysql2"
 
 let connection = mysql.createConnection({
     host: process.env.DB_HOST,
--- server/router/test.ts
+++ server/router/test.ts
@@ -2,7 +2,7 @@ const express = require("express");
 const dbConnection = require("../database");
 
 const router = express.Router();
-import { RowDataPacket } from "mysql2"
+import { QueryError,RowDataPacket } from "mysql2"
 
 export interface User extends RowDataPacket {
   idx: number
@@ -10,20 +10,8 @@ export interface User extends RowDataPacket {
   name: string
 }
 
-
 router.get("/", (req: any, res: any) => {
-    dbConnection.query<User[]>(
-        'SELECT * FROM `user` WHERE `name` = ?',
-        ['lee'],
-        function(err, results, fields) {
-          console.log(results); // results contains rows returned by server
-          console.log(fields); // fields contains extra meta data about results, if available
-      
-          // If you execute same statement again, it will be picked from a LRU cache
-          // which will save query preparation time and give better performance
-        }
-      );
-    
+    dbConnection.query('SELECT * FROM `user`', (err: QueryError, results:User[]) => {console.log(results);});
     res.send({ test : "hi"});
 })
 
